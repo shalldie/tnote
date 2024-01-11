5310ca6154ac5e5b30e1cb99930711c68b57b07d
xieshuang <hi.xieshuang@gmail.com>
2024-01-11 18:27:33 +0800

    remove log

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -1,12 +1,5 @@
 package utils
 
-import (
-	"fmt"
-	"log"
-	"os"
-	"time"
-)
-
 // 三元运算
 func Ternary[T any](condition bool, item1 T, item2 T) T {
 	if condition {
@@ -26,23 +19,23 @@ func MathMin(a int, b int) int {
 	return Ternary(a < b, a, b)
 }
 
-func Log(msg string) {
+// func Log(msg string) {
 
-	filePath := "log.out" // 要操作的文件路径
+// 	filePath := "log.out" // 要操作的文件路径
 
-	// 打开文件，如果不存在则创建新文件
-	file, err := os.OpenFile(filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		log.Fatal("无法打开文件", filePath, ": ", err)
-	}
-	defer file.Close()
+// 	// 打开文件，如果不存在则创建新文件
+// 	file, err := os.OpenFile(filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+// 	if err != nil {
+// 		log.Fatal("无法打开文件", filePath, ": ", err)
+// 	}
+// 	defer file.Close()
 
-	contentToAppend := time.Now().Format("2006-01-02 15:04:05") + ": " + msg + "\n" // 要追加的内容
+// 	contentToAppend := time.Now().Format("2006-01-02 15:04:05") + ": " + msg + "\n" // 要追加的内容
 
-	_, err = file.WriteString(contentToAppend)
-	if err != nil {
-		log.Fatal("无法向文件追加内容: ", err)
-	}
+// 	_, err = file.WriteString(contentToAppend)
+// 	if err != nil {
+// 		log.Fatal("无法向文件追加内容: ", err)
+// 	}
 
-	fmt.Println("成功追加内容到文件")
-}
+// 	fmt.Println("成功追加内容到文件")
+// }
